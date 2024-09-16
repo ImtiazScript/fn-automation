df0af54af57f19da0048ed01aa94f1574b50eb96
Imtiaz Ahmed <imtiaz.uap@gmail.com>
2024-09-17 03:25:55 +0600

    using env file for logRetensionDays

--- backend/crons/cleanOlderLogs.js
+++ backend/crons/cleanOlderLogs.js
@@ -7,12 +7,13 @@ cron.schedule('55 */23 * * *', async () => {
     if(process.env.DISABLED_CRONS === 'true') {
         return;
     }
+    const logRetentionDays = process.env.LOG_RETENTION_DAYS ? parseInt(process.env.LOG_RETENTION_DAYS) : 7;
     const currentDateTime = new Date().toLocaleString();
-    logger.info(`CLEAN LOG:: Older than 7 days log deletion cron running at: ${currentDateTime}`);
+    logger.info(`CLEAN LOG:: Older than ${logRetentionDays} days log deletion cron running at: ${currentDateTime}`);
 
     try {
         // Calculate the date 7 days ago
-        const sevenDaysAgo = new Date(new Date() - 7 * 24 * 60 * 60 * 1000);
+        const sevenDaysAgo = new Date(new Date() - logRetentionDays * 24 * 60 * 60 * 1000);
 
         // Define the Log model
         const Log = mongoose.models.Log || mongoose.model('Log', new mongoose.Schema({}, { strict: false, collection: 'server_logs' }));
@@ -21,9 +22,9 @@ cron.schedule('55 */23 * * *', async () => {
         const result = await Log.deleteMany({ timestamp: { $lt: sevenDaysAgo } });
 
         if (result.deletedCount > 0) {
-            logger.info(`CLEAN LOG:: Successfully deleted ${result.deletedCount} logs older than 7 days at: ${currentDateTime}`);
+            logger.info(`CLEAN LOG:: Successfully deleted ${result.deletedCount} logs older than ${logRetentionDays} days at: ${currentDateTime}`);
         } else {
-            logger.info(`CLEAN LOG:: No logs found older than 7 days at: ${currentDateTime}`);
+            logger.info(`CLEAN LOG:: No logs found older than ${logRetentionDays} days at: ${currentDateTime}`);
         }
     } catch (error) {
         logger.error(`CLEAN LOG:: Failed to delete logs: ${error.message}`);
